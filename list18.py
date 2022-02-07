d3edbfaf2d08bb8fe62918e1f55843868a6be26f
Mark Lacerenze <marklacer@gmail.com>
2022-02-07 10:19:35 -0300

    Update

---
+++ list18.py
@@ -0,0 +1,25 @@
+"""
+Programa que vaya llenando dos listas de numeros, una 
+lista de positivos y otra de negativos. 
+Entonces se debe imprimir el vector (sólo los elementos introducidos).
+"""
+
+positive_list = []
+negative_list = []
+element = 0 
+opt = 's'
+
+while opt == 's':
+  element = int(input('Enter a number: '))
+  if element >= 0:
+    positive_list.append(element)
+  else:
+    negative_list.append(element)
+    
+  opt = str(input('Continue?: "s" or "n" -> '))
+  if opt == 's':
+    continue
+  else:
+    break
+  
+print(f'Positive List -> {positive_list} \nNegative List -> {negative_list}')
